1887781f0d1d71c33f92e8e5aecb8c1393b49c7f
fangyuan <fangyuan@mail.jj.cn>
2019-09-11 15:35:21 +0800

    commit framebuffer shader files.

--- winPlayer/winPlayer/res/shader/FrameBufferTestShader.fs
+++ winPlayer/winPlayer/res/shader/FrameBufferTestShader.fs
@@ -7,5 +7,6 @@ uniform sampler2D texture1;
 
 void main()
 {
-    FragColor = texture(texture1, TexCoords);
+    //FragColor = texture(texture1, TexCoords);
+	FragColor =	vec4(vec3(1.0 - texture(texture1, TexCoords)), 1.0);
 }
